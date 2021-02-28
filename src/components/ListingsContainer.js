78b9e894e1efeeb2125d4375ea1a39445077425e
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-28 17:09:48 -0500

    mmmmm

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,49 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ search, listings, setListings }) {
+  const [sortBy, setSortBy] = useState("");
+  const filteredListings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(search.toLowerCase())
+  );
+  const allListings = filteredListings.map((listing) => {
+    return (
+      <ListingCard
+        key={listing.id}
+        id={listing.id}
+        description={listing.description}
+        image={listing.image}
+        location={listing.location}
+        listings={listings}
+        setListings={setListings}
+      />
+    );
+  });
+
+  function sortListings(e) {
+    setSortBy(e.target.value);
+    const sortedListings = filteredListings.sort((listing1, listing2) => {
+      if (!sortBy) {
+        return 0;
+      } else {
+        return listing1[sortBy].localeCompare(listing2[sortBy]);
+      }
+    });
+    return setListings(sortedListings);
+  }
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <select onChange={sortListings}>
+        <option select hidden>
+          Sort By
+        </option>
+        <option value="description"> Description</option>
+        <option value="location"> Location</option>
+        {/* <option value="price"> price</option> */}
+      </select>
+
+      <ul className="cards">{allListings}</ul>
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-function Search() {
+function Search({ search, setSearch }) {
   function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
   }
-
   return (
     <form className="searchbar" onSubmit={handleSubmit}>
       <input
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
